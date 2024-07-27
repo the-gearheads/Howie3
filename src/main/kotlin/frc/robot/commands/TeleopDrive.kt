b5176a7fafd9d399bcc146598e3941adc6b66380
Ceticx0 <gavinoperry9@gmail.com>
2024-07-27 17:08:13 -0400

    destroy deprecation warning

--- src/main/kotlin/frc/robot/commands/TeleopDrive.kt
+++ src/main/kotlin/frc/robot/commands/TeleopDrive.kt
@@ -1,6 +1,6 @@
 package frc.robot.commands
 
-import edu.wpi.first.wpilibj2.command.CommandBase
+import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Subsystem
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj.Joystick
@@ -11,7 +11,7 @@ import frc.robot.subsystems.ArmSpinner
 
 const val DEADBAND = 0.02
 
-class TeleopDrive(val drive: Drive, val joy: XboxController, val arm: ArmSpinner): CommandBase() {
+class TeleopDrive(val drive: Drive, val joy: XboxController, val arm: ArmSpinner): Command() {
 
     init {
       addRequirements(drive)
