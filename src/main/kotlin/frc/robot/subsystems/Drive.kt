0f644e35270088d730a2768ffdbb21147acfe7df
Ceticx0 <gavinoperry9@gmail.com>
2024-10-24 19:29:59 -0400

    change motor ids

--- src/main/kotlin/frc/robot/subsystems/Drive.kt
+++ src/main/kotlin/frc/robot/subsystems/Drive.kt
@@ -15,10 +15,10 @@ import frc.robot.commands.TeleopDrive
 class Drive: SubsystemBase {
   constructor(): super()
 
-  private val leftFront = CANSparkMax(1, MotorType.kBrushless)
-  private val leftRear = CANSparkMax(2, MotorType.kBrushless)
-  private val rightFront = CANSparkMax(3, MotorType.kBrushless)
-  private val rightRear = CANSparkMax(4, MotorType.kBrushless)
+  private val leftFront = CANSparkMax(2, MotorType.kBrushless)
+  private val leftRear = CANSparkMax(3, MotorType.kBrushless)
+  private val rightFront = CANSparkMax(4, MotorType.kBrushless)
+  private val rightRear = CANSparkMax(5, MotorType.kBrushless)
  
   private val diff = DifferentialDrive(leftFront, rightFront)
 
